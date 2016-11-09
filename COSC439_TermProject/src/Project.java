9ec90bb9ec469fbd3df91d4355834f3ce6bba71b
Robert Gott <robgott@yahoo.com>
2016-11-09 18:50:19 -0500

    Basic project with finding integer

---
+++ COSC439_TermProject/src/Project.java
@@ -0,0 +1,10 @@
+
+public class Project
+{
+	public static void main(String[] args)
+	{
+		CombinationValidation b = new CombinationValidation(100, 10, 1000);
+		
+		System.out.println(b.StartFind());
+	}
+}
