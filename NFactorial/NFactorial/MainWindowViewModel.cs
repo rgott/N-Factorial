c77fc9f6bc21519479bf5819dc98529aa4698618
Robert Gott <robgott@yahoo.com>
2017-04-29 00:05:54 -0400

    Basic Layout of java app

---
+++ NFactorial/NFactorial/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFactorial
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string NumberCountPerThread { get; set; }
+        public string ThreadCount { get; set; }
+        public string MaxElementQueueCount { get; set; }
+        public string FactorialInput { get; set; }
+        public string FactorialOutput { get; set; }
+        public string FactorialOutputLength { get; set; }
+        public string TimeTaken { get; set; }
+
+        public string PartialProgressStatus { get; set; }
+        public int PartialProgressValue { get; set; }
+        public int PartialProgressMaximum { get; set; }
+
+        public string TotalProgressStatus { get; set; }
+        public int TotalProgressValue { get; set; }
+        public int TotalProgressMaximum { get; set; }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
